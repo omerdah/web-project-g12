17f0d2cdbbd2312f14fe9ca4256eb3ed30c1176f
omerdah <88436135+omerdah@users.noreply.github.com>
2021-09-09 17:29:42 +0300

    Add files via upload

---
+++ HomePage.js
@@ -0,0 +1,19 @@
+
+//window.onscroll = function() {scrollingNavBar()};
+
+
+
+
+function scrollingNavBar() {
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+
+
